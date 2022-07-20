e88298037d237c9f4ecf02a2c03aef5a26aa13d3
awe peter <awe.petersq@gmail.com>
2022-07-20 15:18:04 -0700

    service card

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var toggleSwitch = document.querySelector(".toggle-switch");
+
+toggleSwitch.addEventListener("change",() =>{
+  if(toggleSwitch.checked){
+      document.querySelector(".pricing-main").classList.add("active");
+  }
+  else{
+      document.querySelector(".pricing-main").classList.remove("active");
+  }
+});
