863e70eecb4b4381cbd50c253e21560f797acc2f
awe peter <awe.petersq@gmail.com>
2022-07-28 02:44:15 -0700

    notification bar

--- js/app.js
+++ js/app.js
@@ -9,3 +9,34 @@ toggleSwitch.addEventListener("change",() =>{
   }
 });
 
+jQuery(document).ready(function($){
+	//create the slider
+	$('.cd-testimonials-wrapper').flexslider({
+		selector: ".cd-testimonials > li",
+		animation: "slide",
+		controlNav: false,
+		slideshow: false,
+		smoothHeight: true,
+		start: function(){
+			$('.cd-testimonials').children('li').css({
+				'opacity': 1,
+				'position': 'relative'
+			});
+		}
+	});
+});
+
+document.querySelector(".banner__close").addEventListener("click", function () {
+  this.closest(".banner").style.display = "none";
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("banner").style.top = "0";
+  } else {
+    document.getElementById("banner").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
